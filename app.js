b4e9fd5efd55c91e124147ec646a7f51f59dd45f
hawarijawed <hawarijawed143@gmail.com>
2025-02-06 19:19:19 +0530

    new updates

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const path = require('path');
 const ownersRoute = require('./routes/ownerRoute');
 const usersRoute = require('./routes/usersRoute');
 const productsRoute = require('./routes/productsRoute');
-const expressSession = require("express-session");
-const flash = require("connect-flash");
+// const expressSession = require("express-session");
+// const flash = require("connect-flash");
 const db = require('./config/mongoose-connection');//connects the databse
 
 
@@ -16,19 +16,22 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-app.use(
-    expressSession({
-        resave:false,
-        saveUninitialized: false,
-        secret: process.env.EXPRESS_SESSION_SECRET,
-    })
-);
-app.use(flash);
+// app.use(
+//     expressSession({
+//         resave:false,
+//         saveUninitialized: false,
+//         secret: process.env.EXPRESS_SESSION_SECRET,
+//     })
+// );
+// app.use(flash);
 app.use(express.static(path.resolve('./public')));
 app.set("view engine", "ejs");
 
 //Handling user routes
-
+app.get("/", (req, res)=>{
+    //res.send("Aur batao");
+    res.render("shop");
+});
 app.use("/owners", ownersRoute);
 app.use("/users", usersRoute);
 app.use("/products", productsRoute);
